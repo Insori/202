23fcf3fd38638546980cf7b2f21a332666485a19
apf_temp_admin <s2110@e-mirim.hs.kr>
2022-03-18 20:41:17 +0900

    std:: 안 적어도 됨

--- 202/C++기본/기본.cpp
+++ 202/C++기본/기본.cpp
@@ -1,11 +1,12 @@
 //cout, endl 객체를 가지고 있음
 #include <iostream>
 
+using namespace std;	//std:: 따로 안 적어도 됨
 int main(void)
 {
 	//printf를 cout 객체로
 	//endl 객체로 개행
-	std::cout << "안녕 세상아" << std::endl;
+	cout << "안녕 세상아" << endl;
 
 	return 0;
 }
