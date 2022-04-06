a14815ed4d76d3c2743ab510b68a453e3136d4e5
apf_temp_admin <s2110@e-mirim.hs.kr>
2022-04-06 10:06:43 +0900

    Call by 래퍼런스

--- 202/C++기본/02-04 Call by 래퍼런스.cpp
+++ 202/C++기본/02-04 Call by 래퍼런스.cpp
@@ -2,15 +2,15 @@
 
 using namespace std;
 
-void plus2(int* num)
+void plus2(int& num)
 {
-	*num += 2;
+	num += 2;
 }
 
 int main(void)
 {
 	int a = 3;
-	plus2(&a);
+	plus2(a);
 	cout << a << endl;
 
 	return 0;
