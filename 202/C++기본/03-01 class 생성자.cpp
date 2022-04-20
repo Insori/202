77aa88372306a09b2cf112bb888e2a449d97e0f6
apf_temp_admin <s2110@e-mirim.hs.kr>
2022-04-20 09:41:59 +0900

    멤버함수 추가

--- 202/C++기본/03-01 class 생성자.cpp
+++ 202/C++기본/03-01 class 생성자.cpp
@@ -8,16 +8,30 @@ class Student
 private:	
 	int nHakbun;
 	string sName;
+	
+public:
 
 	//생성자: 객체가 생성될 때 호출되는 함수
-	//디폴트 생성자를 컴파일러가 만들어준다
-	//Student(){}
+	Student(void)
+	{
+		nHakbun = 1234;
+		sName = "나재민";
+		cout << "학번이 등록되었습니다." << endl;
+	}
+
+	void show(void)
+	{
+		cout << "학번은 " << nHakbun << "입니다." << endl;
+		cout << "이름은 " << sName << "입니다." << endl << endl;
+	}
 };
 
 int main(void)
 {
 	Student stu1;
-	Student stu2;
+	stu1.show();
+	Student stu2 = Student();
+	stu2.show();
 
 
 	return 0;
