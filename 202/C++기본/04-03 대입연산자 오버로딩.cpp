e8e5a2849a1215f74ca9eeb85cd7800058afd523
apf_temp_admin <s2110@e-mirim.hs.kr>
2022-06-08 09:40:02 +0900

    클래스 바깥으로 옮기기

--- 202/C++기본/04-03 대입연산자 오버로딩.cpp
+++ 202/C++기본/04-03 대입연산자 오버로딩.cpp
@@ -20,19 +20,7 @@ public:
 	~Student();
 
 	//연산자 오버로딩
-	Student& operator=(const Student& rhs)
-	{
-
-		cout << "대입연산자 호출" << endl;
-		//기존에 존재하는 공간을 제거하고 새 공간 할당 준비
-		delete[]sName;
-		nHakbun = rhs.nHakbun;
-		int len = strlen(rhs.sName) + 1;		//공간 개수 측정
-		sName = new char[len];		//공간 생성
-		strcpy(sName, rhs.sName);
-
-		return *this;
-	}
+	Student& operator=(const Student& rhs);
 
 	void show(void);
 };
@@ -79,6 +67,20 @@ Student::Student(const Student& rhs)
 	int len = strlen(rhs.sName) + 1;	
 	sName = new char[len];
 	strcpy(sName, rhs.sName);
+}
+
+Student& Student::operator=(const Student& rhs)
+{
+
+	cout << "대입연산자 호출" << endl;
+	//기존에 존재하는 공간을 제거하고 새 공간 할당 준비
+	delete[]sName;
+	nHakbun = rhs.nHakbun;
+	int len = strlen(rhs.sName) + 1;		//공간 개수 측정
+	sName = new char[len];		//공간 생성
+	strcpy(sName, rhs.sName);
+
+	return *this;
 }
 
 Student::~Student()
