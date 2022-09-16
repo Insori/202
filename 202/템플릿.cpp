ef83285e8e46d42f9cdf7d5ef1caaa14b61b7df4
apf_temp_admin <s2110@e-mirim.hs.kr>
2022-09-16 23:47:28 +0900

    템플릿 예제 변경

--- 202/템플릿.cpp
+++ 202/템플릿.cpp
@@ -3,30 +3,31 @@
 
 using namespace std;
 
+template <typename T1, typename T2>
 class Character {
 public: 
-	Character(string name, int hp, int attack, int defense) {
-		this->name = name;
-		this->hp = hp;
-		this->attack = attack;
-		this->defense = defense;
+	Character(T1 sd, T2 id1, T2 id2, T2 id3) {
+		sdata = sd;
+		idata1 = id1;
+		idata2 = id2;
+		idata3 = id3;
 	}
 	void print() {
-		cout << "이름: " << name << endl;
-		cout << "체력: " << hp << endl;
-		cout << "공격: " << attack << endl;
-		cout << "방어: " << defense << endl;
+		cout << "이름: " << sdata << endl;
+		cout << "체력: " << idata1 << endl;
+		cout << "공격: " << idata2 << endl;
+		cout << "방어: " << idata3 << endl;
 	}
 
 private:
-	string name;
-	int hp;
-	int attack;
-	int defense;
+	T1 sdata;
+	T2 idata1;
+	T2 idata2;
+	T2 idata3;
 };
 
 int main(void) {
-	Character* c = new Character("Andy", 100, 30, 20);
+	Character<string, int>* c = new Character<string, int>("Andy", 100, 30, 20);
 	c->print();
 
 	delete c;
