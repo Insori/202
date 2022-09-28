3e14fe0343cc28cf69a216534210f61a4367db3c
apf_temp_admin <s2110@e-mirim.hs.kr>
2022-09-28 10:19:20 +0900

    iterator(반복자) 활용

--- 202/08-01 동적 배열 vector.cpp
+++ 202/08-01 동적 배열 vector.cpp
@@ -6,30 +6,32 @@ using namespace std;
 
 int main(void) {
 
-	//크기 확장이 가능한 동적배열
-	vector<string> s3;
-	s3.reserve(10);	//크기를 10개로 확장
+    //크기 확장이 가능한 동적배열
+    vector<string> s3;
+    s3.reserve(10);   //크기를 10개로 확장
 
-	for (int i = 0; i < 10; i++) {
-		s3.push_back("ab");
-	}
+    for (int i = 0; i < 10; i++) {
+        s3.push_back("ab");
+    }
 
-	//기존 공간에서 크기를 확장하여 삽입
-	s3.push_back("ab");
+    //기존 공간에서 크기를 확장하여 삽입
+    s3.push_back("ab");
 
+    vector<string>::iterator iter;
 
-	for (int i = 0; i < s3.size(); i++) {
-		cout << s3[i] << " ";
-	}
-	cout << endl;
+    //벡터의 원소들을 출력(iterator)
+    for (iter = s3.begin(); iter != s3.end(); iter++) {
+        cout << *iter << " ";
+    }
+    cout << endl;
 
-	s3[0] = "cd";
-	s3.at(2) = "ef";
+    s3[0] = "cd";
+    s3.at(2) = "ef";
 
-	for (int i = 0; i < s3.size(); i++) {
-		cout << s3[i] << " ";
-	}
-	cout << endl;
+    for (iter = s3.begin(); iter != s3.end(); iter++) {
+        cout << *iter << " ";
+    }
+    cout << endl;
 
-	return 0;
+    return 0;
 }
